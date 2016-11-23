f6f146e0f5e2e088809c9532bd931262d86215db
vitkarpov <viktor.s.karpov@gmail.com>
2016-11-23 18:50:49 +0300

    events

--- examples/test.js
+++ examples/test.js
@@ -1,14 +1,32 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var state = {
+    name: 'hello'
+};
+
 var App = React.createClass({
+    onClick() {
+        state.name = "hello2";
+        render();
+    },
     render() {
         return (
             <div className="app">
-                {this.props.name}
+                <div>
+                    My name is {this.props.name}
+                </div>
+                <button onClick={this.onClick}>
+                    change name
+                </button>
+                <input ref="myInput" placeholder="enter your name" />
             </div>
         );
     }
 });
 
-ReactDOM.render(<div className="wrapper"><App name="hello" /></div>, document.getElementById('app'));
+function render() {
+    ReactDOM.render(<App name={state.name} />, document.getElementById('app'));
+}
+
+render();
